91199c75c191b1cf14c524f1643f5370a25bf168
JM4P1R0 <juanmanuelpinor@gmail.com>
2024-03-25 22:56:30 -0500

    new info and new icon

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
 import './style.css'
+
+
+let table = document.getElementById("information-rows")
+
+table.innerHTML = ""
+
+
+const tableRow = ( names, last_names, permissions) => {
+    console.log( names, last_names, permissions );
+    return `<tr>
+        <td>${names}</td>
+        <td>${last_names}</td>
+        <td>${permissions.create ?"X" :"" }</td>
+        <td>${permissions.upload ?"X" :"" }</td>
+        <td>${permissions.delete ?"X" :"" }</td>
+        <td>
+        No sé
+        <i class="bi bi-trash"></i>
+        </td>
+    </tr>`
+}
+
+
+console.log( table );
+
+fetch("/data/rols.json")
+    .then( data => data.json() )
+    .then( json => {
+
+        json.forEach(element => {
+            let {names, last_names, permissions} = element
+            table.innerHTML += tableRow( names, last_names, permissions )
+        });
+        console.log( json ) 
+    })
