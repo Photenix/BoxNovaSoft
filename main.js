db3ad4639f4bfba140c7c200927345adeb7f9403
JM4P1R0 <juanmanuelpinor@gmail.com>
2024-04-04 21:56:21 -0500

    back

--- main.js
+++ main.js
@@ -4,12 +4,24 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 
 let table = document.getElementById("information-rows")
 
+let moduleChange = document.getElementById("info-change")
+
 table.innerHTML = ""
 
+const addValuesInfoChange = ( name, lname, chC, chU, chD) => {
+    document.getElementById("name-change").value = name
+    document.getElementById("last-name-change").value = lname
+
+    document.getElementById("create-change").checked = chC == "X"
+    document.getElementById("update-change").checked = chU == "X"
+    document.getElementById("delete-change").checked = chD == "X"
+}
 
 const tableRow = ( names, last_names, permissions) => {
-    console.log( names, last_names, permissions );
-    return `<tr>
+
+    let node = document.createElement("tr")
+
+    node.innerHTML = `
         <td>${names}</td>
         <td>${last_names}</td>
         <td>${permissions.create ?"X" :"" }</td>
@@ -19,7 +31,30 @@ const tableRow = ( names, last_names, permissions) => {
             <i class="bi bi-trash" title="eliminar"></i>
             <i class="bi bi-plus-circle" title="actualizar"></i>
         </td>
-    </tr>`
+    `
+
+    node.querySelector( ".bi.bi-plus-circle" ).addEventListener( "click", evn => {
+
+        let parent = evn.target.parentElement.parentElement
+        
+        let childrens = parent.querySelectorAll("td")
+
+        let info = []
+
+        for (let i = 0; i < childrens.length-1; i++) {
+            const element = childrens[i];
+            info.push( element.innerText );
+        }
+    
+        addValuesInfoChange( ...info );
+        // name.value = names
+
+        moduleChange.showModal()
+    })
+
+
+    // console.log( names, last_names, permissions );
+    return node
 }
 
 
@@ -31,7 +66,8 @@ fetch("/data/rols.json")
 
         json.forEach(element => {
             let {names, last_names, permissions} = element
-            table.innerHTML += tableRow( names, last_names, permissions )
+            // table.innerHTML += tableRow( names, last_names, permissions )
+            table.appendChild( tableRow( names, last_names, permissions ) )
         });
         console.log( json ) 
     })
